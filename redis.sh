a8aba59f1a40333d5966267542210fc3219f162b
amer-devops <amerahmed.official@gmail.com>
2025-09-27 17:58:42 +0530

    roboshop

--- redis.sh
+++ redis.sh
@@ -5,13 +5,13 @@ dnf module enable redis:7 -y
 VALIDATE $? "Enable redis 7"
 
 dnf install redis -y 
-VALIDATE $? "Installing redis"
+VALIDATE $? "Install redis"
 
 sed -i 's/127.0.0.1/0.0.0.0/g' /etc/redis/redis.conf
 VALIDATE $? "Allowing remote connections to redis"
 
 systemctl enable redis
-VALIDATE $? "Enabliling redis"
+VALIDATE $? "Enable redis"
 
 systemctl start redis 
-VALIDATE $? "starting redis"
+VALIDATE $? "start redis"
