becd347f21bcaf086e3de4d0fc8d82d8876961c4
amer-devops <amerahmed.official@gmail.com>
2025-09-27 13:30:54 +0530

    roboshop

--- catalogue.sh
+++ catalogue.sh
@@ -43,7 +43,7 @@ else
     echo "User alredy exist...$Y SKIPPING $N"
 fi
 
-mkdir /app 
+mkdir -P /app 
 VALIDATE $? "Creating app directory"
 
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip &>>$LOG_FILE
