972fecd1f3eb279b7b90b4d202f3dafa633776f8
amer-devops <amerahmed.official@gmail.com>
2025-09-27 19:20:16 +0530

    roboshop

--- cart.sh
+++ cart.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
-cartID=$(id -u)
+USERID=$(id -u)
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
-LOGS_FOLDER="/var/log/shell-script"
+LOGS_FOLDER="/var/log/shell-roboshop"
 SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
-LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" # /var/log/shell-script/16-logs.log
-MONGO_HOST=mongodb.daws86s.icu
 SCRIPT_DIR=$PWD
+MONGODB_HOST=mongodb.daws86s.icu
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" # /var/log/shell-script/16-logs.log
+
 mkdir -p $LOGS_FOLDER
 echo "Script started executed at: $(date)" | tee -a $LOG_FILE
 
@@ -21,67 +22,53 @@ fi
 
 VALIDATE(){ # functions receive inputs through args just like shell script args
     if [ $1 -ne 0 ]; then
-        echo -e "Installing $2 ... $R FAILURE $N" | tee -a $LOG_FILE
+        echo -e "$2 ... $R FAILURE $N" | tee -a $LOG_FILE
         exit 1
     else
-        echo -e "Installing $2 ... $G SUCCESS $N" | tee -a $LOG_FILE
+        echo -e "$2 ... $G SUCCESS $N" | tee -a $LOG_FILE
     fi
 }
 
-
+##### NodeJS ####
 dnf module disable nodejs -y &>>$LOG_FILE
-VALIDATE $? "Disable nodejs"
+VALIDATE $? "Disabling NodeJS"
+dnf module enable nodejs:20 -y  &>>$LOG_FILE
+VALIDATE $? "Enabling NodeJS 20"
+dnf install nodejs -y &>>$LOG_FILE
+VALIDATE $? "Installing NodeJS"
 
-dnf module enable nodejs:20 -y &>>$LOG_FILE
-VALIDATE $? "Enable nodejs 20"
-
-dnf install nodejs -y
-VALIDATE $? "Install nodejs 20"
-
-id=roboshop
+id roboshop &>>$LOG_FILE
 if [ $? -ne 0 ]; then
-    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop
-    VALIDATE $? "Adding system user"
+    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOG_FILE
+    VALIDATE $? "Creating system user"
 else
-    echo -e "User alredy exist...$Y SKIPPING $N"
+    echo -e "User already exist ... $Y SKIPPING $N"
 fi
 
-rm -rf /app
-VALIDATE "Removing existing directory"
-
-mkdir /app 
+mkdir -p /app
 VALIDATE $? "Creating app directory"
 
-
-
 curl -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart-v3.zip &>>$LOG_FILE
-VALIDATE $? "Downloading cart"
+VALIDATE $? "Downloading cart application"
 
 cd /app 
-VALIDATE $? "change app directory"
+VALIDATE $? "Changing to app directory"
 
 rm -rf /app/*
-VALIDATE "Removing existing code"
+VALIDATE $? "Removing existing code"
 
 unzip /tmp/cart.zip &>>$LOG_FILE
-VALIDATE $? "Unzip cart"
-
-cd /app 
-VALIDATE $? "change app directory"
+VALIDATE $? "unzip cart"
 
 npm install &>>$LOG_FILE
-VALIDATE $? "Installing dependencies"
+VALIDATE $? "Install dependencies"
 
 cp $SCRIPT_DIR/cart.service /etc/systemd/system/cart.service
-VALIDATE $? "Copy cart.services"
+VALIDATE $? "Copy systemctl service"
 
 systemctl daemon-reload
-
 systemctl enable cart &>>$LOG_FILE
 VALIDATE $? "Enable cart"
 
-systemctl start cart
-VALIDATE $? "start cart"
-
 systemctl restart cart
-VALIDATE $? "Restarting cart"
+VALIDATE $? "Restarted cart"
