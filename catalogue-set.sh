8448b4858aad6c325b9173c5688b853cd3d043d5
amer-devops <amerahmed.official@gmail.com>
2025-09-29 23:06:03 +0530

    roboshop

--- catalogue-set.sh
+++ catalogue-set.sh
@@ -31,6 +31,7 @@ dnf install nodejs -y &>>$LOG_FILE
 echo -e "Installing NodeJS 20 ... $G SUCCESS $N"
 
 id roboshop &>>$LOG_FILE
+
 if [ $? -ne 0 ]; then
     useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOG_FILE
 else
