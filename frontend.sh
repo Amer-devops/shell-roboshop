0ccf42d2072b312d476394656ab643c3ba8d6385
amer-devops <amerahmed.official@gmail.com>
2025-09-29 22:30:05 +0530

    roboshop

--- frontend.sh
+++ frontend.sh
@@ -39,12 +39,16 @@ systemctl start nginx
 VALIDATE $? "Starting Nginx"
 
 rm -rf /usr/share/nginx/html/* 
+
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip &>>$LOG_FILE
+
 cd /usr/share/nginx/html 
+
 unzip /tmp/frontend.zip &>>$LOG_FILE
 VALIDATE $? "Downloading frontend"
 
 rm -rf /etc/nginx/nginx.conf
+
 cp $SCRIPT_DIR/nginx.conf /etc/nginx/nginx.conf
 VALIDATE $? "Copying nginx.conf"
 
