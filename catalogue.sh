3d417d787224478824d5e37877ba90af294f0344
amer-devops <amerahmed.official@gmail.com>
2025-09-30 10:50:00 +0530

    roboshop

--- catalogue.sh
+++ catalogue.sh
@@ -9,7 +9,7 @@ N="\e[0m"
 LOGS_FOLDER="/var/log/shell-script"
 SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" # /var/log/shell-script/16-logs.log
-MONGO_HOST=mongodb.daws86s.icu
+MONGODB_HOST=mongodb.daws86s.icu
 SCRIPT_DIR=$PWD
 mkdir -p $LOGS_FOLDER
 echo "Script started executed at: $(date)" | tee -a $LOG_FILE
@@ -89,6 +89,9 @@ dnf install mongodb-mongosh -y &>>$LOG_FILE
 VALIDATE $? "Install mongodb client"
 
 mongosh $MONGODB_HOST --quiet --eval "db.getMongo().getDBNames().indexOf('catalogue')"
+
+rm -rf //app/db/master-data.js*
+VALIDATE "Removing existing code" 
 
 if [ $INDEX -le 0 ]; then
     mongosh --host $MONGODB_HOST </app/db/master-data.js &>>$LOG_FILE
